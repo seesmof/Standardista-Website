5c6f780b6f8ae138b6f365fa77311a6d5b1855ce
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-25 12:43:57 +0000

    Tuesday, April 25, 2023, 3:43:57 PM Eastern European Summer Time

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,5 +24,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-children"),
+  ],
 };
