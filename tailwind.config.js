146e9199aff3a432a12fe22c5688ce18dbca60f5
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-25 12:43:24 +0000

    Tuesday, April 25, 2023, 3:43:24 PM Eastern European Summer Time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,28 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  mode: "jit",
+  purge: ["./**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./*.{html,js}"],
   theme: {
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
+    extend: {},
   },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: false,
+    themes: false,
+    base: false,
+    utils: false,
+    logs: false,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+    themes: false,
+  },
+};
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
