4653cf015ce068802ba9f76e555ec7495b40ea87
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-25 12:07:15 +0000

    Tuesday, April 25, 2023, 3:07:15 PM Eastern European Summer Time

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,13 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#0FCFEC",
-          secondary: "#19D3AE",
-          accent: "#3A4256",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-        },
-      },
-      "dark",
-      "cupcake",
-    ],
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
   },
 };
